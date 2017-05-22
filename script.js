923ee442c873b3719269af2fcafafcfd35cd8b4f
alexander "sandy" gilfillan <alexander.l.gilfillan@gmail.com>
2017-05-21 19:12:27 -0700

    lanes and sublanes populate accurately

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const validate = (type, o) => {
   if (type === 'player') {
     const playerName = o.newPlayer.value;
 
+    if (!playerName) {
+      isValid = false;
+      return;
+    }
+
     if ((o.board.players.length + 1) > 5) {
       isValid = false;
       window.alert('Too many players! Must be 1 to 5.');
@@ -24,6 +29,12 @@ const getRandomIntInclusive = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+const setAttributes = (el, attrs) => {
+  for (let key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+}
+
 class ScoreBoard {
   constructor() {
     this.players = [];
@@ -43,12 +54,10 @@ class ScoreBoard {
       board.players.push(player);
 
       const scoreboard = document.getElementById('scoreboard');
-      const id = `${player.name}Lane`;
       const lane = player.createScoreLane();
       scoreboard.appendChild(lane);
 
       newPlayer.value = '';
-
     }
   }
 }
@@ -63,23 +72,37 @@ class Player {
 
   createScoreLane() {
     const lane = document.createElement('div');
-    lane.setAttribute('id', `${this.name}Lane`);
-    lane.setAttribute('class', 'row');
+    const attrs = { id: `${this.name}Lane`, class: 'lane' };
+    setAttributes(lane, attrs);
 
     for (let i = 0; i <= 10; i++) {
-      const col = document.createElement('div');
+      const round = document.createElement('div');
+
       if (i === 0) {
-        col.textContent = `${this.name}`;
-        col.setAttribute('class', 'row');
+        setAttributes(round, { class: 'playerName' });
+        round.textContent = `${this.name}`;
       } else {
-        col.textContent = `col${i}`;
-        col.setAttribute('id', `col${i}`);
-        col.setAttribute('class', 'col');
+        const attrs = { id: `round${i}`, class: 'round', name: `round${this.name}` };
+        setAttributes(round, attrs);
+        round.textContent = `${i}`;
       }
-      lane.appendChild(col);
+      lane.appendChild(round);
     }
 
+    this.createSubLanes(lane);
     return lane;
+  }
+
+  createSubLanes(lane) {
+    lane.childNodes.forEach((round, idx) => {
+      for (let i = 0; i <= 2; i++) {
+        if (idx === 0) break;
+        const roll = document.createElement('div');
+        setAttributes(roll, { id: `round${idx}roll${i}`, class: 'roll' });
+        if (i < 2) round.appendChild(roll);
+        if (i === 2 && idx === 10) round.appendChild(roll);
+      }
+    });
   }
 
   roll() {
